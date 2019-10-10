b691f7b7aa84e3ef7e71ba50332540bd36a54a00
KLattimore <keithlat22@gmail.com>
2019-10-10 19:18:11 -0400

    edited ticket class

---
+++ POSApp/Class1.cs
@@ -0,0 +1,56 @@
+﻿Public Class Ticket ()
+{ 
+
+
+    public string Fooditem;
+    public int serverID;
+    public double Price;
+    public int WaitTime;
+    public int TicketID;
+
+public Food_one(string Burger)
+{
+    this.Fooditem = Burger;
+    serverID = 1;
+    Price = 9.99;
+    WaitTime = 15;
+
+}
+public Food_two(string Fries)
+{
+    this.Fooditem = Fries;
+    serverID = 2;
+    Price = 2.49;
+    WaitTime = 7;
+
+    }
+
+public Food_three(string Shake)
+{
+    this.Fooditem = Shake;
+    serverID = 3;
+    Price = 3.99;
+    WaitTime = 5;
+
+    }
+
+public Food_four(string Chicken_Sand)
+{
+    this.Fooditem = Chicken_Sand;
+    serverID = 4;
+    Price = 8.49;
+    WaitTime = 12;
+
+    }
+
+public Food_five(string Salad)
+{
+    this.Fooditem = Salad;
+    serverID = 5;
+    Price = 11.25;
+    WaitTime = 6;
+
+    }
+
+}
+
